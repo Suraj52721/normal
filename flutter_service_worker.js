ea6ed91a0beb51c4444a290a474f9449a323e40d
Suraj Singh <suraj.52721@gmail.com>
2023-12-07 13:06:43 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,8 +24,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "3c209836cecca4663dedb03c6cd06bd0",
-"/": "3c209836cecca4663dedb03c6cd06bd0",
+"index.html": "da572ab2620bef335f59f7017cc7bd5d",
+"/": "da572ab2620bef335f59f7017cc7bd5d",
 "main.dart.js": "309436eff2688e4b7883afd5a05a63be",
 "manifest.json": "cfb692dc75a3dc04c36e6cd310d7734b",
 "version.json": "99dba51f05bbad9065072ac1a336ebbf"};
